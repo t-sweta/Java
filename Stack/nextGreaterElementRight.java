1a6c80ccbae4462befabaad3c77b06b1784cc3ac
t-sweta <swetashreya004@gmail.com>
2023-09-15 19:00:00 +0530

    Next Greater Element to Right Using Stack

--- Stack/nextGreaterElementRight.java
+++ Stack/nextGreaterElementRight.java
@@ -4,19 +4,19 @@ import java.util.*;
 
 public class nextGreaterElementRight {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
     int[] arr = { 1, 4, 3, 7, 8, 2, 6 };
     int[] res = nextGreaterRight(arr);
+
     for (int i = 0; i < arr.length; i++) {
       System.out.print(res[i] + " ");
     }
-
   }
 
   public static int[] nextGreaterRight(int[] arr) {
     int[] ans = new int[arr.length];
     Arrays.fill(ans, -1);
     Stack<Integer> st = new Stack<>();
+
     for (int i = arr.length - 1; i >= 0; i--) {
       while (!st.empty() && st.peek() < arr[i]) {
         st.pop();
@@ -27,7 +27,6 @@ public class nextGreaterElementRight {
       st.push(arr[i]);
     }
     return ans;
-
   }
 
 }
