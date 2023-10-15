2932ca4e8bbdd93a97941260b2660d92b1239f47
t-sweta <swetashreya004@gmail.com>
2023-10-15 16:12:56 +0530

    Changes Made in Code

--- Recursion/printReverseNNo.java
+++ Recursion/printReverseNNo.java
@@ -1,22 +1,22 @@
 import java.util.*;
+
 public class printReverseNNo {
-  public static void print(int n ){
-    if(n==1){
+  public static void print(int n) {
+    if (n == 1) {
       System.out.println(n);
       return;
     }
     System.out.println(n);
-    print(n-1);
+    print(n - 1);
     return;
   }
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     print(n);
     sc.close();
 
-
-    
   }
-  
+
 }
