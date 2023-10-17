05bd0944b51a24e404b2bc0484d2e9b5850bcaca
t-sweta <swetashreya004@gmail.com>
2023-10-17 22:16:08 +0530

    String Compression Method 2 - Using HashMap

--- ETPractice/ET1StringCompression.java
+++ ETPractice/ET1StringCompression.java
@@ -4,11 +4,12 @@ public class ET1StringCompression {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     String s = sc.next();
-    System.out.println(compress(s));
-
+    // System.out.println(compress(s)); USING METHOD 1
+    System.out.println(compression(s));
   }
 
   private static String compress(String s) {
+    // METHOD 1 (WITHOUT USING HASHMAP)
     int n = s.length();
     String ans = "";
     int j = 1;
@@ -29,6 +30,33 @@ public class ET1StringCompression {
     ans = ans + s.charAt(i) + count;
 
     return ans;
+  }
+
+  private static String compression(String s) {
+    // USING HASHMAP(linkedhashmap as it maintains the order as it is)
+    StringBuilder sb = new StringBuilder();
+
+    LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+
+    for (int i = 0; i < s.length(); i++) {
+
+      char ch = s.charAt(i);
+      if (map.containsKey(ch)) {
+        int count = map.get(ch);
+        count++;
+        map.put(ch, count);
+
+      } else {
+        map.put(ch, 1);
+      }
+    }
+    for (Map.Entry<Character, Integer> e : map.entrySet()) {
+      sb.append(e.getKey());
+      sb.append(e.getValue());
+
+    }
+    return sb.toString();
+
   }
 
 }
